49097da022c0c516616c64232269c60106e8a480
wangyonglong666 <767311790@qq.com>
2025-04-05 23:17:27 +0800

    完成Task3

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,11 +1,14 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
 
-    /** Returns the total sum in a list of integers */
+    /**
+     * Returns the total sum in a list of integers
+     */
     public static int sum(List<Integer> L) {
         // TODO: Fill in this function.
-        if(L.isEmpty()) {
+        if (L.isEmpty()) {
             return 0;
         } else {
             int sum = 0;
@@ -13,25 +16,63 @@ public class ListExercises {
                 sum += n;
             }
             return sum;
+
         }
     }
 
-    /** Returns a list containing the even numbers of the given list */
+    /**
+     * Returns a list containing the even numbers of the given list
+     */
     public static List<Integer> evens(List<Integer> L) {
         // TODO: Fill in this function.
-        return null;
+        List<Integer> ret = new ArrayList<>();
+        for (int n : L) {
+            if (n % 2 == 0) {
+                ret.add(n);
+            }
+        }
+        return ret;
     }
 
-    /** Returns a list containing the common item of the two given lists */
+    /**
+     * Returns a list containing the common item of the two given lists
+     */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
         // TODO: Fill in this function.
-        return null;
+        List<Integer> ret = new ArrayList<>();
+        for (int i : L1) {
+            for (int j : L2) {
+                if (i == j) {
+                    ret.add(i);
+                }
+            }
+        }
+        return ret;
     }
 
 
-    /** Returns the number of occurrences of the given character in a list of strings. */
+    /**
+     * Returns the number of occurrences of the given character in a list of strings.
+     */
     public static int countOccurrencesOfC(List<String> words, char c) {
         // TODO: Fill in this function.
-        return 0;
+        //错误版本
+//        int count = 0;
+//        for (String n : words) {
+//            if (n.contains(c)) {
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
+        int count = 0;
+        for (String word : words) {
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) == c) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
