26d3936f6879fabd6c04f3850a80b60bfa4db404
wangyonglong666 <767311790@qq.com>
2025-04-06 11:32:12 +0800

    完成hw0b

--- hw0b/src/Dessert.java
+++ hw0b/src/Dessert.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Dessert {
+    public int flavor;//味道
+    public int price;//价格
+    public static int numDessert;//迄今为止做出来甜点的数量
+
+    public Dessert(int f, int p) {
+        this.flavor = f;
+        this.price = p;
+        Dessert.numDessert++;
+    }
+
+    public void printDessert() {
+
+        System.out.println(this.flavor+" "+this.price+" "+Dessert.numDessert);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("I love dessert!");
+    }
+
 }
