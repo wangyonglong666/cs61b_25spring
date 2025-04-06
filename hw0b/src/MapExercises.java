bbf6cecd0b084485b508c787346d2b5bb4d7015f
wangyonglong666 <767311790@qq.com>
2025-04-06 11:15:03 +0800

    完成Task 3: MapExercises

--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,7 +8,13 @@ public class MapExercises {
      */
     public static Map<Character, Integer> letterToNum() {
         // TODO: Fill in this function.
-        return null;
+        Map<Character, Integer> ret = new HashMap<>();
+        int n = 1;
+        for (char c = 'a'; c <= 'z'; c++) {
+            ret.put(c, n);
+            n++;
+        }
+        return ret;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
@@ -15,12 +22,26 @@ public class MapExercises {
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
         // TODO: Fill in this function.
-        return null;
+        Map<Integer, Integer> ret = new HashMap<>();
+        for (int n : nums) {
+            ret.put(n, n * n);
+        }
+        return ret;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
         // TODO: Fill in this function.
-        return null;
+        Map<String,Integer> ret = new HashMap<>();
+        for (String word : words) {
+            int count = 0;
+            for (String w : words) {
+                if (word.equals(w)) {
+                    count++;
+                }
+            }
+            ret.put(word,count);
+        }
+        return ret;
     }
 }
