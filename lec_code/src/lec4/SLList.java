f529c3a569d144b5f032fd3a5b479d61e5272bd7
wangyonglong666 <767311790@qq.com>
2025-04-12 23:18:43 +0800

    完成task2

--- lec_code/src/lec4/SLList.java
+++ lec_code/src/lec4/SLList.java
@@ -1,3 +1,5 @@
+package lec4;
+
 public class SLList {
     private static class IntNode {
         int item;
@@ -9,18 +11,67 @@ public class SLList {
         }
     }
 
-    private IntNode first;
+    private IntNode sentinel;
+    private int size;
 
     public SLList(int x) {
-        this.first = new IntNode(x, null);
+        this.sentinel = new IntNode(5498, null);
+        this.sentinel.next = new IntNode(x,null);
+        size = 1;
     }
 
+    public SLList() {
+        this.sentinel = new IntNode(5498,null);
+        this.size = 0;
+    }
+
+
+    //头插法
     public void addFirst(int x) {
-        first = new IntNode(x, this.first);
+        sentinel.next = new IntNode(x, this.sentinel.next);
+        size++;
     }
 
     public int getFirst() {
-        return this.first.item;
+        return sentinel.next.item;
+    }
+
+    //尾插法
+    public void addLast(int x) {
+        size++;
+        IntNode p = this.sentinel;
+        while (p.next != null) {
+            p = p.next;
+        }
+        p.next = new IntNode(x,null);
+        size++;
+    }
+    //size()实现
+    //1.迭代版本
+//    public int size() {
+//        IntNode p = this.first;
+//        int count = 1;
+//        while (p.next != null ) {
+//            p = p.next;
+//            count++;
+//        }
+//        return count;
+//    }
+    //2.递归版本
+//    public int size() {
+//        return size(this.first);
+//    }
+//
+//    private int size(IntNode i) {
+//        if (i == null) {
+//            return 0;
+//        } else {
+//            return 1 + size(i.next);
+//        }
+//    }
+    //3.最快速的方法
+    public int size() {
+        return this.size;
     }
 
 }
